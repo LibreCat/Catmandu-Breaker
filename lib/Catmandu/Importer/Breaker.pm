d93535d1677c1f131d4efbd881d2374b9b7354c5
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2016-04-07 16:42:04 +0200

    Adding support for record based output

--- lib/Catmandu/Importer/Breaker.pm
+++ lib/Catmandu/Importer/Breaker.pm
@@ -1,25 +1,35 @@
 package Catmandu::Importer::Breaker;
 
 use Catmandu::Sane;
-
-our $VERSION = '0.01';
-
 use Moo;
 use namespace::clean;
 
+our $VERSION = '0.01';
+
 with 'Catmandu::Importer';
+
+has record => (is => 'ro', default => sub { 0 });
 
 sub generator {
     my ($self) = @_;
     sub {
-        state $count   = 0;
+        state $prev_id;
         state $line;
+        state $data = {};
 
         while ( defined( $line = $self->fh->getline ) ) {
             chomp $line;
             next if $line =~ /^\s*#/;
 
             my ($id,$field,$values) = split(/\s+/,$line,3);
+
+            my $copy;
+
+            if ($self->record && defined($prev_id) && $id ne $prev_id) {
+                # Set a copy
+                $copy = {%$data};
+                $data = {};
+            }
 
             my ($namespace,$tag);
 
@@ -28,18 +38,36 @@ sub generator {
                 $tag = $2;
             }
 
-            my $data = {
-                _id       => $id ,
-                field     => $field ,
-                namespace => $namespace ,
-                tag       => $tag ,
-                data      => $values , 
-            };
+            if ($self->record) {
+                $data->{_id} = $id;
+                push @{$data->{field}}      , $field;
+                push @{$data->{namespace}} , $namespace;
+                push @{$data->{tag}}       , $tag;
+                push @{$data->{data}}      , $values;
 
-            return $data;
+                $prev_id = $id;
+
+                return $copy if defined $copy;
+            }
+            else {
+                return {
+                    _id       => $id ,
+                    field     => $field ,
+                    namespace => $namespace ,
+                    tag       => $tag ,
+                    data      => $values , 
+                };
+            }
         }
 
-        return;
+        if (defined $data) {
+            my $copy = {%$data};
+            $data = undef;
+            return $copy;
+        }
+        else {
+            return undef;
+        }
     };
 }
 
@@ -58,8 +86,11 @@ Catmandu::Importer::Breaker - Package that imports the Breaker format
     # Import from a OAI-PMH repository
     $ catmandu convert OAI --url http://biblio.ugent.be/oai to Breaker > data.breaker
     
-    # Convert the Breaker format into JSON
+    # Convert the Breaker format by line into JSON
     $ catmandu convert Breaker < data.breaker
+
+    # Convert the Breaker format by record into JSON
+    $ catmandu convert Breaker --record 1 < data.breaker
 
 =head1 DESCRIPTION
 
@@ -67,9 +98,50 @@ Inspired by the article "Metadata Analysis at the Command-Line" by Mark Phillips
 L<http://journal.code4lib.org/articles/7818> this exporter breaks a OAI-PMH DC harvest
 into the Breaker format which can be analyzed further by command line tools.
 
-=head1 BREAKER FORMAT
+=head1 CONFIGURATION
+
+=over
+
+=item file
+
+Read input from a local file given by its path. Alternatively a scalar
+reference can be passed to read from a string.
+
+=item fh
+
+Read input from an L<IO::Handle>. If not specified, L<Catmandu::Util::io> is used to
+create the input stream from the C<file> argument or by using STDIN.
+
+=item encoding
+
+Binmode of the input stream C<fh>. Set to C<:utf8> by default.
+
+=item fix
+
+An ARRAY of one or more fixes or file scripts to be applied to imported items.
+
+=item record 
+
+The to a true value to join all record values in an array see C<BREAKER OUTPUT FORMAT>
+
+=back
+
+=head1 BREAKER INPUT FORMAT
 
    <record-identifier><tab><metadata-field><tab><metadata-value>
+
+=head1 BREAKER OUTPUT FORMAT
+
+The breaker format is parsed into a Hash containing 4 fields:
+
+    _id:   the idenifier of the record
+    field: the full name (plus namespace) of a field
+    namespace: the namespace of a field
+    tag:   the name of a field
+    data:  the content of the field
+
+When the C<record> option is set to a true value than the field,namespace,tag
+and data will contain an array of all possible values in a record.
 
 =head1 SEE ALSO
 
